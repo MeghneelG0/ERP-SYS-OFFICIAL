deaa6103afda61bc0a268178621f584a5982ce63
Arunabha-NOTE <arunabham2017@gmail.com>
2025-07-08 13:30:11 +0530

    refactor: added shared types and changed prisma schema

--- packages/db/prisma/migrations/20250418181944_add_kpi_fields/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterTable
-ALTER TABLE "assigned_kpi" ADD COLUMN     "form_input" JSONB,
-ADD COLUMN     "kpi_description" TEXT,
-ADD COLUMN     "kpi_value" DOUBLE PRECISION;
-
--- AlterTable
-ALTER TABLE "kpi" ADD COLUMN     "kpi_description" TEXT,
-ADD COLUMN     "kpi_value" DOUBLE PRECISION;
