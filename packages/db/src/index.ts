e3674b4963e9ddf5ba2a4a1938ac10f5b69246d8
Arunabha-NOTE <arunabham2017@gmail.com>
2025-07-21 14:05:09 +0530

    fix: prisma

---
+++ packages/db/src/index.ts
@@ -0,0 +1,3 @@
+import { PrismaClient } from "../prisma/client";
+export { prisma } from "./client"; // exports instance of prisma
+export * from "./client"; // exports generated types and client from prisma
