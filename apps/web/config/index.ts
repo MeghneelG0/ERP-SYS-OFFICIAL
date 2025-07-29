1fda9c37ef448462a9c7fd29e99e17b9aaa19462
Arunabha-NOTE <arunabham2017@gmail.com>
2025-07-29 15:11:18 +0530

    fix: vercel deployment fix

--- apps/web/config/index.ts
+++ apps/web/config/index.ts
@@ -1,6 +1,9 @@
 export const envConfig = {
   appUrl: process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000",
-  apiUrl: process.env.NEXT_PUBLIC_API_URL || "http://localhost:3001",
+  apiUrl:
+    process.env.NODE_ENV === "production"
+      ? `${process.env.NEXT_PUBLIC_APP_URL}/api/server`
+      : process.env.NEXT_PUBLIC_API_URL || "http://localhost:3001",
   nextAuthSecret: process.env.NEXTAUTH_SECRET || "my-secret",
   nextAuthUrl: process.env.NEXTAUTH_URL || "http://localhost:3000",
 };
